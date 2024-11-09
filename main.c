451bc1178fd7ab18aa8f893baf9ff381d275958f
amelli1 <146226107+amelli1@users.noreply.github.com>
2024-11-09 15:18:02 -0800

    Fixing Default if filename is wrong
    This fixes the errors the TA talked about.

--- main.c
+++ main.c
@@ -8,32 +8,38 @@
  * Evan Brown
  * 
 * This is the test branch
+* gcc main.c -o test -DDEBUG (to run debug mode with prints)
+* gcc main.c -o test (run without prints)
+* ./test (run code)
+* ./test rwims.din (to run with a file name)
 */
 
-
-// Header file inclusion
 
 #include <stdio.h>
 #include <stdlib.h>
 
-// Defines and Globals
-
-
-
-// Function prototypes
-
-
-
 int main(int argc, char *argv[]) {
     // Set default filename
-    const char *filename = "Default.din";
+    const char *default_filename = "Default.din";
+    const char *filename;
 
     // Check if user provided a filename as an argument
     if (argc > 1) {
         filename = argv[1];
+    } else {
+        filename = default_filename;
     }
 
     FILE *file = fopen(filename, "r");
+
+    // If the provided file can't be opened, try the default file
+    if (file == NULL && argc > 1) {
+        fprintf(stderr, "The '%s' file provided could not be opened. Attempting to open default file '%s'.\n", filename, default_filename);
+        filename = default_filename;
+        file = fopen(filename, "r");
+    }
+
+    // Check if the file (either provided or default) was successfully opened
     if (file == NULL) {
         perror("Error opening file");
         return 1;
@@ -50,7 +56,6 @@ int main(int argc, char *argv[]) {
         printf("Operation: %d, Address: 0x%X\n", operation, address);
         #endif
         // Process the values here if needed
-
     }
 
     fclose(file);  // Close the file
