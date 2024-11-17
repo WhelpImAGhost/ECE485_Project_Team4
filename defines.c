b8c83ed85f3235ffdd6984edde1b7aa0c63ff489
Caleb Monti <76889116+WhelpImAGhost@users.noreply.github.com>
2024-11-16 18:05:19 -0800

    Implemented cache structure

---
+++ defines.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <math.h>
+
+/* MESI Types */
+#define INVALID 0b00	/* Data is invalid */
+#define SHARED 0b01	/* Data is shared in other caches */
+#define EXCLUSIVE 0b10	/* This cache is exclusive owner */
+#define MODIFIED 0b11	/* This cache has a modified version of the data */
+
+/* Snoop Result Types*/
+#define NOHIT 0b00		/* No hit */
+#define HIT 0b01		/* Hit */
+#define HITM 0b10		/* Hit to modified liner */
+
+/* Bus Operation Types */
+#define READ 0b00		/* Bus Read */
+#define WRITE 0b01		/* Bus Write */
+#define INVALIDATE 0b10	/* Bus Invalidate */
+#define RWIM 0b11		/* Bus Read with Intent to Modify */
+
+/* Current to Higher Cache Message Types */
+#define REQUEST 0b00	/* Request a modified line from higher cache */
+#define SEND 0b01		/* Send a requested line to higher cache */
+#define INVALIDATE 0b10	/* Invalidate a line in higher cache */
+#define EVICT 0b11		/* Evict a line in higher cache */
+
+/* Address Operations */
+#define READ_HD 0b0000		/* Read request from higher data cache */
+#define WRITE_HD 0b0001		/* Write request from higher data cache */
+#define READ_HI 0b0010		/* Read request from higher instruction cache */
+#define READ_S 0b0011		/* Snoop read request */
+#define WRITE_S 0b0100		/* Snoop write request */
+#define RWIM_S 0b0101		/* Snoop read with intent to modify request */
+#define INVALIDATE_S 0b0110	/* Snoop invalidate command */
+#define CLEAR 0b0111		/* Clear the cache and reset all states */
+#define PRINT 0b1000		/* Print content and state of each valid cache line */
+
+/* Memory Size Defaults */
+#define CACHE_SIZE 26		/* 2^(CACHE_SIZE) = True Capacity in Bytes */
+#define CACHE_LINE_SIZE 64	/* Number of bytes per line */
+#define ADDRESS_SIZE 32		/* Number of bits in address (no need to change) */
+#define ASSOCIATIVITY 16		/* Number of ways per set */
+
+/* Tag Array Sizes */
+#define TAG_ARRAY_MESI 2
+
--- main.c
+++ main.c
@@ -17,8 +17,107 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "defines.c"
+
+uint64_t counter = 0;
 
 int main(int argc, char *argv[]) {
+
+    
+    // Add flags for setting non-default variables
+
+
+
+    const int address_size = ADDRESS_SIZE;
+
+    /* Memory Size Calculations */
+
+    const int TRUE_CAPACITY = pow(2,CACHE_SIZE);
+    const int LINES = (TRUE_CAPACITY / CACHE_LINE_SIZE);
+    const int SETS = (LINES / ASSOCIATIVITY);
+
+
+    /* Tag Array Calculations */
+
+
+    const int BYTE_SELECT_BITS = log2(CACHE_LINE_SIZE);
+    const int INDEX_BITS = log2(SETS);
+    const int TAG_BITS = address_size - (BYTE_SELECT_BITS + INDEX_BITS);
+    const int PLRU_ARRAY_SIZE = (ASSOCIATIVITY - 1);
+    const int TOTAL_TAG_ARRAY = SETS * ((ASSOCIATIVITY * (TAG_BITS + TAG_ARRAY_MESI)) + PLRU_ARRAY_SIZE); 	
+    //Can be used for testing to verify that values can be changed correctly
+
+    // Include tests for if the tag is too big or index is too big
+    // (go to default if the input values are out of bounds). 
+
+
+    // Set up cache arrays
+    
+    typedef struct {
+        int mesi;                   // One per way
+        int tag;
+    } Way;
+
+
+    typedef struct {
+        int plru[PLRU_ARRAY_SIZE]; // One per Set
+        Way *ways[ASSOCIATIVITY];                  // One per set
+
+    } Set;
+
+
+    
+    Set *index[SETS];
+    // Allocate memory for index and its components
+
+
+    for (int i = 0; i < SETS; i++) {
+
+
+        
+        index[i] = (Set *)malloc(sizeof(Set));
+        if (index[i] == NULL) {
+            perror("Failed to allocate memory for Set");
+            exit(EXIT_FAILURE);
+        }
+
+        for (int l = 0; l < PLRU_ARRAY_SIZE; l++) {
+            index[i]->plru[l] = 0;
+        }
+
+        
+        for (int k = 0; k < ASSOCIATIVITY; k++) {
+
+            index[i]->ways[k] = (Way *)malloc(sizeof(Way));
+            if (index[i]->ways[k] == NULL) {
+                perror("Failed to allocate memory for Way");
+                exit(EXIT_FAILURE);
+            }
+
+            index[i]->ways[k]->mesi = INVALID;
+
+            
+                counter++;
+                index[i]->ways[k]->tag = 0;
+        }
+        
+    }
+
+    #ifdef DEBUG
+    fprintf(stderr, "Number of sets: %d\n", SETS);
+    fprintf(stderr, "Total number of initialized Ways: %llu\n", counter);
+    #endif
+
+    for (int i = 0; i < ASSOCIATIVITY; i++){
+        index[0]->ways[i]->tag = 15 - i;
+        // index[0]->ways[i]->tag
+        // index[0]->ways[i]->tag
+    }
+
+    for (int i = 0; i < ASSOCIATIVITY; i++){
+        fprintf(stderr, "Tag val of way %d in set 0: %d\n", i, index[0]->ways[i]->tag);
+    }
+
     // Set default filename
     const char *default_filename = "Default.din";
     const char *filename;
@@ -45,7 +144,7 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    printf("Using file: %s\n", filename);
+    fprintf(stderr, "Using file: %s\n", filename);
 
     int operation;
     unsigned int address;
@@ -53,7 +152,7 @@ int main(int argc, char *argv[]) {
     // Read each line until end of file
     while (fscanf(file, "%d %x", &operation, &address) == 2) {
         #ifdef DEBUG
-        printf("Operation: %d, Address: 0x%X\n", operation, address);
+        fprintf(stderr, "Operation: %d, Address: 0x%X\n", operation, address);
         #endif
         // Process the values here if needed
     }
