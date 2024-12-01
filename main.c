44e77a64391292b572c75a8cbe0520257ea6b693
chriskp-pdx <chrisk@pdx.edu>
2024-11-30 22:11:29 -0800

    s

--- main.c
+++ main.c
@@ -266,7 +266,7 @@ int main(int argc, char *argv[]) {
                 #endif
                 SnoopChecker(index, set_index, tag);
                     if(mode){
-                        //printf("\nBusRd @ 0x%08X, Snoop Result: %s sent\n", address, SendSnoopResult);
+                        printf("\nBusRd @ 0x%08X, L2 Snoop Result: %s sent\n", address, snoop_reply);
                     }
                 break;
 
