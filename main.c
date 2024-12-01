daae71bc0582d9bfbc1ddd97c96b7910a8554e1b
Caleb Monti <76889116+WhelpImAGhost@users.noreply.github.com>
2024-11-30 18:26:15 -0800

    fixed

--- main.c
+++ main.c
@@ -220,9 +220,9 @@ int main(int argc, char *argv[]) {
                 #ifdef DEBUG
                     fprintf(stderr, "Case 2\n");
                 #endif
-                CacheResult = hit_or_miss(index, set_index, tag);
+                CacheResult = hit_or_miss(index, set_index, tag, mesi_state);
                 if (CacheResult) {
-                    if(mode){ printf("PrRd HIT @ 0x%08X, %c\n", address, ); //TODO add MESI bits
+                    if(mode){ printf("PrRd HIT @ 0x%08X, %c\n", address,mesi_state ); //TODO add MESI bits
                     }
                 }else {
                     if(mode){ 
