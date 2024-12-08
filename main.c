dbd1eb06338523b9f57bb0593eb76dfa7db145cf
chriskp-pdx <chrisk@pdx.edu>
2024-12-07 19:49:09 -0800

    Update main.c

--- main.c
+++ main.c
@@ -341,7 +341,7 @@ int main(int argc, char *argv[]) {
                         if (old_mesi_state == MODIFIED){
                             printf("\nPrRd CAPACITY MISS @ 0x%08X\n", address);
                             printf("L2: EVICTLINE 0x%08X\n", (old_address));
-                            printf("L2: FlushWB @ 0x%08X, L2 MESI State: INVALID\n", (old_address)); //Writeback Old Address
+                            printf("L2: FlushWB @ 0x%08X, L2 MESI State: INVALID\n", (old_address));
                             printf("L2: BusRd @ 0x%08X, Snoop Result: %s, MESI State: %s\n", (address & ~(0x3F)), snoop_state, mesi_state);
                             if(strcmp(snoop_state,"HITM")==0){
                                 printf("Snooped Operation: FLushWB @0x%08X\n", address);
